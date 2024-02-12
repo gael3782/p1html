5f9eb95033f98e818814d78da3836d6920b988ff
gael3782 <mendozadamian@gmail.com>
2024-02-12 11:30:56 -0600

    agrege nueva tabla

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,4 +1,10 @@
 function changeColor(){ 
   document.getElementById("palabrita").style.backgroundColor="green";
 
+    document.getElenmentById("palabrita").textContent="hola";
+
+}
+
+ function enviarMsg(){
+   aler("hola amigo mio");
 }
